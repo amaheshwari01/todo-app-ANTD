864527c8ac34f4c4aa2b2850665e8b8fe5306837
amaheshwari01 <acoding2007@gmail.com>
2023-03-25 13:13:26 -0700

    convert radio to ANTD

--- frontend/src/components/form/radio.jsx
+++ frontend/src/components/form/radio.jsx
@@ -3,20 +3,21 @@ import { useEffect, useContext } from "react";
 import MainContext from "../../contexts/main";
 
 import apiService from "../../services/api.service";
-
-const Radio = ({ itemId, checked = false }) => {
+import { Radio } from 'antd';
+const Radioa = ({ itemId, checked = false }) => {
 
   const { setTodos } = useContext(MainContext);
 
-  const completeTodo = async() => {
+  const completeTodo = async () => {
     await apiService.updateTodo(itemId, { "is_completed": !checked });
     const response = await apiService.getTodo();
     setTodos(response.data);
     if (!checked) document.getElementById("audio").play();
   };
-  return <div className={checked ? "app-radio-checked" : "app-radio"} onClick={() => completeTodo()}>
-    { checked && <span/> }
-  </div>;
+  return <Radio checked={checked} onClick={() => completeTodo()}></Radio>
+  // <div className={checked ? "app-radio-checked" : "app-radio"} onClick={() => completeTodo()}>
+  //   {checked && <span />}
+  // </div>;
 };
 
-export default Radio;
+export default Radioa;
--- frontend/src/components/todo/item.jsx
+++ frontend/src/components/todo/item.jsx
@@ -7,23 +7,23 @@ import * as Semantic from "semantic-ui-react";
 import MainContext from "../../contexts/main";
 
 import apiService from "../../services/api.service";
-import Radio from "../form/radio";
+import Radioa from "../form/radio";
 
 const Item = ({ item }) => {
 
-  const [ showControls, setShowControls ] = useState(false);
+  const [showControls, setShowControls] = useState(false);
   const {
     setTodos,
     setTodo,
     setUpdate
   } = useContext(MainContext);
 
-  const getTodos = async() => {
+  const getTodos = async () => {
     const response = await apiService.getTodo();
     setTodos(response.data);
   };
 
-  const deleteTodo = async() => {
+  const deleteTodo = async () => {
     await apiService.deleteTodo(item._id);
     await getTodos();
   };
@@ -32,7 +32,7 @@ const Item = ({ item }) => {
   return <div className="app-item"
     onMouseEnter={() => setShowControls(true)}
     onMouseLeave={() => setShowControls(false)}>
-    <Radio checked={item.is_completed} itemId={item._id}/>
+    <Radioa checked={item.is_completed} itemId={item._id} />
     <div className="app-item-text" >
       <span style={{ textDecoration: item.is_completed ? "line-through" : "none" }}>{item.content}</span>
       <div style={{
@@ -47,10 +47,10 @@ const Item = ({ item }) => {
           setUpdate(item._id);
           setTodo(item.content);
         }}>
-          <Semantic.Icon name="edit" color="yellow"/>
+          <Semantic.Icon name="edit" color="yellow" />
         </span>
         <span onClick={() => deleteTodo()}>
-          <Semantic.Icon name="delete" color="red"/>
+          <Semantic.Icon name="delete" color="red" />
         </span>
       </div>
     }
